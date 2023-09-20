3476d70d5af5e9df0d095e4948f0683949665bbc
dreadnought-admin <dom.alexan@gmail.com>
2023-09-19 21:29:38 -0600

    Enables CORS

--- aldapi/aldapi/settings.py
+++ aldapi/aldapi/settings.py
@@ -52,6 +52,12 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
+
+]
+
+CORS_ALLOWED_ORIGINS = [
+    'http://localhost:3000',
 ]
 
 ROOT_URLCONF = 'aldapi.urls'
